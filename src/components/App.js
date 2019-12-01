36b79bfaaa5b8b21d09f3189ead2090498a839df
Aron Fischer <aronfischersigl@googlemail.com>
2019-12-01 19:21:19 +0100

    finish music-master

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import Artist from "./Artist";
+import Tracks from "./Tracks";
+import Search from "./Search";
 
 const API_ADDRESS = "https://spotify-api-wrapper.appspot.com";
 
 class App extends Component {
   state = {
-    artistQuery: "",
     artist: null,
     tracks: []
   };
+
+  componentDidMount() {
+    this.searchArtist("Bruno Mars");
+  }
 
   updateArtistQuery = event => {
     this.setState({ artistQuery: event.target.value });
@@ -20,8 +25,8 @@ class App extends Component {
     }
   };
 
-  searchArtist = () => {
-    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+  searchArtist = artistQuery => {
+    fetch(`${API_ADDRESS}/artist/${artistQuery}`)
       .then(response => response.json())
       .then(json => {
         if (json.artists.total > 0) {
@@ -42,13 +47,9 @@ class App extends Component {
     return (
       <div>
         <h2>Music Master</h2>
-        <input
-          onChange={this.updateArtistQuery}
-          onKeyPress={this.handleKeyPress}
-          placeholder="Search for an Artist"
-        />
-        <button onClick={this.searchArtist}>Search</button>
+        <Search searchArtist={this.searchArtist} />
         <Artist artist={this.state.artist} />
+        <Tracks tracks={this.state.tracks} />
       </div>
     );
   }
